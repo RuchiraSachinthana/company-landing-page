b60e1e46180598ca1d7964287bae5f4a5bbd25a1
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-10-31 18:33:04 +0530

    edited

---
+++ application/views/home.php
@@ -0,0 +1,47 @@
+
+
+   
+<?php include "parts/nav.php" ?>
+
+
+
+
+
+
+
+
+
+
+
+   <hr>
+
+
+
+
+
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, unde velit vitae possimus, minima sed eius impedit temporibus debitis est tempore dicta modi nihil quo porro id itaque doloribus, asperiores.
+Ut voluptatibus quaerat quod vitae, voluptatem fugit facilis illum, nostrum voluptates vel, tempore nihil veniam illo repellendus rerum, sapiente saepe accusamus! Consequuntur, accusantium neque aliquam veniam sapiente minus consequatur. Ea.
+Iure explicabo iusto magni reprehenderit. Odit, aperiam. Dolorum amet aspernatur in hic, aut ab tempore delectus veritatis accusamus natus sit quos. Quis veritatis, blanditiis voluptatum et pariatur incidunt debitis porro.
+Eum corrupti quae inventore maiores suscipit, molestias perferendis nam nisi recusandae provident minus voluptatem voluptates natus ipsum laboriosam, ratione veritatis autem quibusdam qui nobis praesentium dignissimos? Est fuga, itaque cum.</p>
+
+<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+</div>
+
+<hr>
+
+
+<div class="container">
+  <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+</div>
+
+ 
+<?php include "parts/footer.php" ?>
